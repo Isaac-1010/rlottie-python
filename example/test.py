64e0665bbccca464e3a68bcb98813c92120f44d4
IsaacGili <isaac@shuffll.com>
2024-06-08 12:30:00 +0300

    Fixed animation last key frame end time

--- example/test.py
+++ example/test.py
@@ -1,10 +1,14 @@
 from rlottie_python import LottieAnimation
 from PIL import Image
 
-anim = LottieAnimation.from_file("example/samples/subtitles-fe.json")
+anim = LottieAnimation.from_file("example/samples/subtitles-fes.json")
 # anim = LottieAnimation.from_file("rlottie/example/resource/Text_Test_HELL_3_RangeSelector.json")
+frames = anim.lottie_animation_get_totalframe()
+print(f"{frames = }")
 
-# Method 1: Saving the frame to file directly
-anim.save_frame("example/output/framefe.png", frame_num=3)
+for i in range(0, frames-1):
+    anim.save_frame(f"example/output/test1/{i}.png", frame_num=i)
+
+
 
 
